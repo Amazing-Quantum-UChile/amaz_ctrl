b7ac824b8bafe5acbc8a5700175283a5ef50c3ad
Victor <victor.gondret@posteo.net>
2026-04-05 16:40:35 -0400

    [Example] Simple scrip

---
+++ src/amaz_ctrl/example/script_client_server/simple_script.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- mode:Python; coding: utf-8 -*-
+
+# ---------------------------------------------------------------------------
+# Created on the Sat Apr 04 2026 by Victor
+# Copyright (c) 2026 - AmazingQuantum@UChile
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------------
+
+'''
+Content of simple_script.py
+
+Please document your code ;-).
+
+'''
+
+
+import threading
+
+class Script:
+    pomme = 100000
+    e=1
+    stop_event = threading.Event() 
+    
+    def set_parameters(self):
+        pass
+    def dacquire(self)->dict:
+        pass
+
+
+
+
+
+
