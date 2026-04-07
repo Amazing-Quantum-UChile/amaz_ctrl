7d4fb57b332564054500f3d33acf9fdac47045d7
Victor <victor.gondret@posteo.net>
2026-04-07 09:58:27 -0400

    [Tools] Adding a logs as new tool

---
+++ src/amaz_ctrl/tools/amaz_logs.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- mode:Python; coding: utf-8 -*-
+
+# ---------------------------------------------------------------------------
+# Created on the Mon Apr 06 2026 by Victor
+# Copyright (c) 2026 - AmazingQuantum@UChile
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------------
+
+'''
+Content of amaz_logs.py
+
+this file define the style of the logs printed in the terminal (or console)
+'''
+
+import logging, colorlog
+
+
+log_formatter_console = colorlog.ColoredFormatter(
+            "%(log_color)s%(levelname)s:%(message)s%(reset)s",
+            log_colors={
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "red",
+            },
+            secondary_log_colors={},
+            style="%",
+        )
+
+def set_console_log(logger_name, log_level="INFO"):
+    """setups the log printed in the console for the server."""
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(log_level)
+    ch = logging.StreamHandler()
+    ch.setFormatter(log_formatter_console)
+    logger.addHandler(ch)
